9e9877c5941a14cd882e9190ec801c4ba2cf8cb9
aike <aaiikkee@gmail.com>
2023-10-20 20:28:21 +0900

    add prev/next function

---
+++ src/20231018.js
@@ -0,0 +1,324 @@
+// 20231018
+// 斜め移動のみ許可したランダムウォーク
+
+let base_h0;
+let base_h;
+let cv;
+
+let floor_is_drawn = false;
+let image_is_drawn = false;
+let frame_is_drawn = false;
+let shadow_is_drawn = false;
+
+const max_grid = 10;
+let grid = Array(max_grid * max_grid);
+let glist = Array(max_grid * max_grid);
+let alist = Array(max_grid * max_grid);
+const nlist = [[-1, -1], [1, -1], 
+               [-1,  1], [1,  1]];
+
+
+function setup() {
+  cv = createCanvas(800, 800);
+  pixelDensity(pixelDensity());
+  angleMode(RADIANS);
+  colorMode(HSB, 360, 100, 100, 100);
+
+  base_h0 = random(360);
+  
+}
+
+function draw() {
+  if (!floor_is_drawn) {
+    floor_is_drawn = true;
+    draw_floor();    
+  }
+
+  let box_size = min(width, height) * 0.5;
+  let xpos = (width - box_size) / 2; 
+  let ypos = (height - box_size) / 2; 
+  let frame_width = 10;
+  let cell_num = 5;
+  
+  
+  if (!image_is_drawn) {
+    draw_image(xpos, ypos, box_size, cell_num);
+    if (!image_is_drawn) {
+      return;
+    }
+  }
+    
+  if (!frame_is_drawn) {
+    frame_is_drawn = true;
+    draw_frame(xpos, ypos, box_size, frame_width);    
+  }
+  
+  if (!shadow_is_drawn) {
+    shadow_is_drawn = true;
+    // smart phone shadow
+    let sc = 1.4;
+    for (let i = 0; i < 10; i++) {
+      shadowmaker(sc - 0.02 * i);
+    }  
+  }
+
+}
+
+///////////////// DRAW IMAGE  ///////////////////////
+
+let dr_i = 0;
+let dr_j = 0;
+
+function draw_image(x, y, size, cell_num) {
+  if (dr_j >= cell_num) {
+    dr_j = 0;
+    dr_i++;
+  }
+  if (dr_i >= cell_num) {
+    image_is_drawn = true;
+    return;
+  }
+  
+  let cell_size = int(size / cell_num);
+  let x0 = x + dr_j * cell_size;
+  let y0 = y + dr_i * cell_size;
+  base_h = int(base_h0 + random(180)) % 360;
+  fillrect(x0, y0, cell_size);
+  // 枠線
+  stroke('#444');
+  strokeWeight(1);
+  noFill();
+  rect(x0, y0, cell_size);
+
+  dr_j++;
+}
+
+function fillrect(x0, y0, s) {
+ 
+  fill('#fff');
+  noStroke();
+  rect(x0, y0, s);
+
+  stroke(base_h, 30, 70);
+    
+  let dv = 6; // dv <= max_grid
+  let mg = s / 6;
+  let ds = (s - mg * 2) / (dv - 1);
+  
+  for (let i = 0; i < dv * dv; i++) {
+    grid[i] = 0;
+    glist[i] = i;
+  }
+
+  for (let y = 0; y < dv; y++) {
+    for (let x = 0; x < dv; x++) {
+      strokeWeight(5);
+      point(x0 + mg + x * ds, y0 + mg + y * ds);
+    }
+  }
+
+  // limited range shuffling 
+  for (let i = 0; i < dv * dv; i++) {
+    item0 = floor(random(dv * dv));
+    item1 = floor(random(dv * dv));
+    itemw = glist[item0];
+    glist[item0] = glist[item1];
+    glist[item1] = itemw;
+  }
+  
+  for (let i = 0; i < dv * dv; i++) {
+    const x = glist[i] % dv;
+    const y = floor(glist[i] / dv);
+
+    let cx = x;
+    let cy = y;
+    if (grid[cy * dv + cx] > 0) continue;
+    grid[cy * dv + cx] = 1;
+    
+    while (true) {
+      let alist_cnt = 0;
+      for (let j = 0; j < nlist.length; j++) {
+        let nx = cx + nlist[j][0];
+        let ny = cy + nlist[j][1];
+        if ((ny < dv) && (nx < dv) && (ny >= 0) && (nx >= 0) && (grid[ny * dv + nx] === 0)) {
+          if (!acrossCheck(x0, y0, cx, cy, nx, ny, mg, ds)) {
+            alist[alist_cnt++] = [nx, ny];
+          }
+        }
+      }
+      if (alist_cnt === 0) break;
+    
+      let r = floor(random(alist_cnt));
+      let nx = alist[r][0];
+      let ny = alist[r][1];
+
+      grid[ny * dv + nx] = 1;
+      strokeWeight(5);
+      let px0 = x0 + mg + cx * ds;
+      let py0 = y0 + mg + cy * ds;
+      let px1 = x0 + mg + nx * ds;
+      let py1 =  y0 + mg + ny * ds;
+      line(px0, py0, px1, py1);     
+      
+      cx = nx;
+      cy = ny;
+
+    }
+  }
+  
+}
+
+function acrossCheck(x0, y0, cx, cy, nx, ny, mg, ds) {
+  // 線の横断チェック
+  let px0 = x0 + mg + cx * ds;
+  let py0 = y0 + mg + cy * ds;
+  let px1 = x0 + mg + nx * ds;
+  let py1 =  y0 + mg + ny * ds;
+  let checkc = get(px0 + (px1 - px0) / 2, py0 + (py1 - py0) / 2);
+  if (checkc.toString() == '255,255,255,255') {
+    return false;
+  }
+  return true;
+}
+
+
+//////////////////////////////////////////////////
+
+function draw_frame(x, y, box_size, frame_size) {
+  // frame
+  strokeWeight(frame_size * 2);
+  noFill();
+  drawingContext.shadowOffsetX = 25;
+  drawingContext.shadowOffsetY = 25;
+  drawingContext.shadowBlur = 40;
+
+  stroke(color(30, 30, 65));
+  drawingContext.shadowColor = '#0005';  
+  rect(x - frame_size, y - frame_size, box_size + frame_size * 2);    
+  drawingContext.shadowColor = '#0000';  
+  for (let i = 0; i < 5; i++) {
+    rect(x - frame_size - i, y - frame_size - i, box_size + frame_size * 2);    
+  }
+  stroke(color(30, 35, 80));
+  rect(x - frame_size - 5, y - frame_size - 5, box_size + frame_size * 2);    
+
+  // frame high light
+  stroke(30, 30, 90);
+  strokeWeight(1);
+  let lx0 = x - frame_size - 15;
+  let ly0 = y - frame_size - 15;
+  let lx1 = lx0 + box_size + 40;
+  let ly1 = ly0;
+  line(lx0, ly0, lx1, ly1); 
+  lx1 = lx0;
+  ly1 = ly0 + box_size + 40;
+  line(lx0, ly0, lx1, ly1); 
+  ly0 = ly0 + 20;
+  ly1 = ly1 - 20;
+  line(lx0 + box_size + 20, ly0, lx0 + box_size + 20, ly1); 
+  ly0 = ly1;
+  line(lx0 + 20, ly0, lx0 + box_size + 20, ly1); 
+}
+
+function draw_floor() {
+  background(color(20, 10, 90));
+
+  // floor
+  let split_num = 5;
+  let h = height / split_num;
+ 
+  for (let i = 0; i < 500; i++) {
+    let y = random(height);
+    let c = 85 + random(8);
+    stroke(color(30, 10, c));
+    line(0, y + 0, width, y + 0);        
+  }
+  
+  // floor seems
+  for (let i = 0; i < split_num; i++) {
+    let y = i * h;
+    stroke(color(30, 10, 70));
+    line(0, y + 0, width, y + 0);    
+    line(0, y + 1, width, y + 1);    
+    stroke(color(30, 10, 95));
+    line(0, y + 2, width, y + 2);    
+    line(0, y + 3, width, y + 3);    
+    
+    if (random() > 0.4) {
+      let x = random(width);
+      stroke(color(30, 10, 70));
+      line(x + 0, y + 0, x + 0, y + h);    
+      line(x + 1, y + 0, x + 1, y + h);    
+      stroke(color(30, 10, 95));
+      line(x + 2, y + 2, x + 2, y + h);    
+      line(x + 3, y + 2, x + 3, y + h);          
+    }
+  }  
+}
+
+function shadowmaker(sc) {
+  colorMode(HSB, 360, 100, 100, 100);
+  
+  fill(color(0, 0, 0, 1));
+  stroke('black');
+  strokeWeight(1);
+  noStroke();
+  
+  beginShape();
+
+  let gx = width;
+  let gy = height;
+  let cx = gx / 2;
+  let cy = gy / 2;  
+  let r = 15;
+  let sc2 = sc - 1;  
+  let x;
+  let y;
+  let w = min(gx,gy) * 0.2 * sc;
+  let h = w * 1.7;
+  
+  x = cx - w / 2;
+  y = cy - w / 2 * sc * 0.5;
+  vertex(x, y + r);
+  vertex(x + r, y);
+  vertex(x + w - r, y);
+  vertex(x + w, y + r);  
+  vertex(x + w, y + h * 0.4);
+  let hand_x = x + w;
+  let hand_y = y;
+  let tx;
+  let ty;
+  let arm_w = 200;
+  tx = hand_x + w * 0.1;
+  ty = hand_y + h * 0.6;
+  vertex(tx, ty);  
+  tx = hand_x - w * 0.05 + 100 * sc2;
+  ty = min(gy * 0.8, y + h * 0.9);
+  vertex(tx, ty);  
+  tx = hand_x - w * 0.06 + 100 * sc2;
+  ty = ty + (gy - ty) / 2;
+  vertex(tx, ty);  
+  tx = hand_x + w * 0.06 + 100 * sc2;
+  ty = gy;
+  vertex(tx, ty);  
+  tx = hand_x - w * 0.2 - 200 * sc2;
+  ty = gy;
+  vertex(tx, ty);  
+  tx = hand_x - w * 0.3 - 200 * sc2;
+  ty = y + (gy - y + h) / 2;
+  vertex(tx, ty);  
+  tx = cx + arm_w / 2 - 100 * sc * 1.2;
+  vertex(tx, y + h);  
+  vertex(x + r, y + h);
+  vertex(x, y + h - r);
+  
+  endShape(CLOSE);  
+}
+
+function keyTyped(){
+  if(key == 's'){
+    let yymmdd = (new Date()).toISOString().replace(/[A-Z].*/,'').replace(/-/g,'');
+    saveCanvas(cv, yymmdd, '');    
+  }
+  return false;
+}
